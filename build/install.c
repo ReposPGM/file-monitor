1326c403731914bdea8066bc43107dde68d7a939
Julio Preciado <38670474+JulioCesarPreciado@users.noreply.github.com>
2023-07-31 16:09:25 -0600

    [Update] comando de choco

--- build/install.c
+++ build/install.c
@@ -7,7 +7,7 @@ void refreshEnv();
 
 int main()
 {
-    const char *choco = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))";
+    const char *choco = "powershell -Command \"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))\"";
     system(choco);
 
     refreshEnv();
